b214b0c722e0fe138453f2d4eede2d0c5211e19d
ch1999ahmad <ch1999ahmad@gmail.com>
2021-04-12 00:28:31 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>wellcome Ahmad ch</Text>
+      <TextInput style={styles.input} placeholder="heloo" />
+      <TouchableOpacity ><Text>heloo</Text></TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input:{backgroundColor:'red',width:'70%',alignSelf:'center',textAlign:'center',}
+});
